6597b6669db5cae9182905f837e747d61197a3ab
xuemeng <xuemeng2@ualberta.ca>
2024-09-28 15:46:11 -0400

    add data in the databse in index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+import pg from 'pg';
+
+// connect to my database
+const db = new pg.Client({
+    user: 'postgres',      
+    host: 'localhost',
+    database: 'TicketSystem',       
+    password: '1234',
+    port: 5432,
+});
+db.connect()
+.then(() => console.log('Connected to PostgreSQL'));
+
 const app = express();
 const port = 3000;
+
 app.get("/", (req, res)=>{
     //res.send("Hello world");
     res.send("<h1>Hello world</h1>"); // sending back html 
