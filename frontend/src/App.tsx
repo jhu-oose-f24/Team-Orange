f6ac7e8cd0384c7446dbfb8fb0fa48f1467cbda0
Brian Johansson <bjohans2@jhu.edu>
2024-10-14 18:35:22 -0400

    Merged with main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -54,7 +54,7 @@ const App: React.FC = () => {
                 }
               />
               <Route path="/profile" element={<Profile />} />
-              <Route path="/" element={<Feed statusFilter="Open"/>} />
+              <Route path="/" element={<Navigate to="/feed" replace/>} />
             </Routes>
           </div>
         </Content>
