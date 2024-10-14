f303bec7c487a0af699961961163557b2a0194a4
jacksonroloff <jroloff1@jh.edu>
2024-10-14 13:40:27 -0400

    new create page

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar: React.FC = () => {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <button>
-                        <Link to="/create-ticket">Create Ticket</Link>
-                    </button>
-                </li>
-                <li>
-                    <button>
-                        <Link to="/feed">Feed</Link>
-                    </button>
-                </li>
-                <li>
-                    <button>
-                        <Link to="/profile">Profile</Link>
-                    </button>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav>
+      <ul>
+        <li>
+          <button>
+            <Link to="/create-ticket">Create Ticket</Link>
+          </button>
+        </li>
+        <li>
+          <button>
+            <Link to="/feed">Feed</Link>
+          </button>
+        </li>
+        <li>
+          <button>
+            <Link to="/profile">Profile</Link>
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
