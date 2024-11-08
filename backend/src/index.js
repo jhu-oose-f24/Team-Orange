f2dd93b234f9908fb5e9cd46b72850ce50da6dd7
xuemeng <xuemeng2@ualberta.ca>
2024-11-08 15:45:13 -0500

    delete onesesion

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const session = require("express-session");
 const bodyParser = require("body-parser");
 const pg = require('pg');
 const cors = require('cors');
