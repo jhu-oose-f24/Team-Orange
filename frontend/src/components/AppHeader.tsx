ef14b458346a82db5340d46614365e783e8e930e
jacksonroloff <jroloff1@jh.edu>
2024-11-13 15:56:43 -0500

    integrate user updates

--- frontend/src/components/AppHeader.tsx
+++ frontend/src/components/AppHeader.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Menu, Layout } from "antd";
-import UsersDropdown from "./UsersDropdown.dev";
 
 const { Header } = Layout;
 
